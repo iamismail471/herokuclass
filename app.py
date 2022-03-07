e53d397d6e8df255c7d92e77b81076e58a4678e7
Ismail J <68530170+iamismail471@users.noreply.github.com>
2022-03-07 18:38:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,32 +2,46 @@ import dash
 import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
  
 USERNAME_PASSWORD_PAIRS = [
-    ['nethu', '12345'],['guvi', 'guvi'],['ismail','ismail'],
+    ['nethu', '12345'],['guvi', 'guvi']
 ]
  
 app = dash.Dash()
 auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
 server = app.server
- 
+
 app.layout = html.Div([
-    dcc.RangeSlider(
-        id='range-slider',
-        min=-5,
-        max=6,
-        marks={i:str(i) for i in range(-5, 7)},
-        value=[-3, 4]
-    ),
-    html.H1(id='product')  # this is the output
-], style={'width':'50%'})
+   html.Div('Convert Temperature'),
+   'Celsius',
+   dcc.Input(
+       id="celsius",
+       value=0.0,
+       type="number"
+   ),
+   ' = Fahrenheit',
+   dcc.Input(
+       id="fahrenheit",
+       value=32.0,
+       type="number",
+   ),
+])
  
 @app.callback(
-    Output('product', 'children'),
-    [Input('range-slider', 'value')])
-def update_value(value_list):
-    return value_list[0]*value_list[1]
+   Output("celsius", "value"),
+   Output("fahrenheit", "value"),
+   Input("celsius", "value"),
+   Input("fahrenheit", "value"),
+)
+def sync_input(celsius, fahrenheit):
+   ctx = dash.callback_context
+   input_id = ctx.triggered[0]["prop_id"].split(".")[0]
+   if input_id == "celsius":
+       fahrenheit= None if celsius is None else (float(celsius) * 9/5) + 32
+   else:
+       celsius = None if fahrenheit is None else (float(fahrenheit) - 32) * 5/9
+   return celsius, fahrenheit
  
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+ app.run_server(debug=True)
